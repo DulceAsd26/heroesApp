e6f9725adb88cddcf45ab9dce9752e15e69d479a
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2020-11-26 02:05:44 -0600

    Eliminando registros con DELETE

--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroeModel } from 'src/app/models/heroe.model';
 import { HeroesService } from 'src/app/services/heroes.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-heroes',
@@ -18,6 +19,24 @@ export class HeroesComponent implements OnInit {
 
     this.heroesService.getHeroes()
       .subscribe( resp => this.heroes = resp);
+
   }
 
+
+  borrarHeroe( heroe: HeroeModel, i: number){
+
+    Swal.fire({
+      title: '¿Está seguro?',
+      text: `¿Está seguro que desea borrar a ${heroe.nombre}?`,
+      icon: 'question',
+      showConfirmButton: true,
+      showCancelButton: true
+    }).then( resp => {
+      if( resp.value){
+    this.heroes.splice(i, 1);
+    this.heroesService.borrarHeroe( heroe.id).subscribe();
+        }
+    });
+
+  }
 }
