7c1c5071b9bb2829593411b7cf19ad6a7260025a
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2020-11-26 00:03:12 -0600

    Actualizando registros con HTTP y PUT

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -25,12 +25,21 @@ export class HeroeComponent implements OnInit {
       return;
     }
     
-  
-    this.heroesService.crearHeroe( this.heroe )
-    .subscribe( resp => {
+
+    if( this.heroe.id ){
+      this.heroesService.actualizarHeroe( this.heroe )
+      .subscribe( resp => {
       console.log(resp);
     });
 
+    }else{
+    this.heroesService.crearHeroe( this.heroe )
+    .subscribe( resp => {
+      console.log(resp);
+      this.heroe = resp;
+    });
+
   }
+}
 
 }
